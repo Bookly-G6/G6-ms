5015de0216715130b1f1bf264074420d14dfd6d0
luciannoFr <luciannotwt@gmail.com>
2026-06-18 13:06:25 -0300

    feat(config): implementar GlobalExceptionHandler para estandarizar respuestas de error

---
+++ src/main/java/com/gotechy/bookly/config/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.gotechy.bookly.config;
+
+import com.gotechy.bookly.core.utils.ErrorResponse;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+        MethodArgumentNotValidException ex
+    ) {
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult()
+            .getAllErrors()
+            .forEach(error -> {
+                String nombreCampo = ((FieldError) error).getField();
+                String mensajeError = error.getDefaultMessage();
+                errores.put(nombreCampo, mensajeError);
+            });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "Ocurrió un error interno en el servidor: " + ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+            error
+        );
+    }
+}
