481bec641c2f678d3b47d9d746f68eabf85c8cf4
luciannoFr <luciannotwt@gmail.com>
2026-06-18 12:15:06 -0300

    feat(catalogo): exponer endpoint POST de productos

---
+++ src/main/java/com/gotechy/bookly/modules/catalogo/controller/ProductoController.java
@@ -0,0 +1,25 @@
+package com.gotechy.bookly.modules.catalogo.controller;
+
+import com.gotechy.bookly.modules.catalogo.dto.ProductoRequestDTO;
+import com.gotechy.bookly.modules.catalogo.model.Producto;
+import com.gotechy.bookly.modules.catalogo.service.ProductoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/productos")
+@RequiredArgsConstructor
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    @PostMapping
+    public ResponseEntity<Producto> crearProducto(
+        @RequestBody ProductoRequestDTO productoDTO
+    ) {
+        Producto nuevoProducto = productoService.crearProducto(productoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoProducto);
+    }
+}
